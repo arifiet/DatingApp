092f89ab279c8f0b9ed601c71839933e55cd4bb3
Mohammad Arif <Mohammad.Arif@Aurigo.com>
2020-01-09 09:51:19 +0530

    End Of Section 8

---
+++ DatingApp.API/Models/Photo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DatingApp.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        public User User { get; set; }   // these user properties are added to have a foreign key relation 
+        public int UserId { get; set; }  // and have a cascade delete if user is deleted , if not included will have restricted delete 
+    }
+}
