9724ff8e54b3938cfcadb3f6333a2c89a1ba03d6
Mohammad Arif <Mohammad.Arif@Aurigo.com>
2020-01-13 20:07:09 +0530

    End of Section 9

---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './../../_models/user';
+import { UserService } from './../../_services/user.service';
+import { AlertifyService } from './../../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+              private route: ActivatedRoute ) { }
+
+  ngOnInit() {
+    // this.loadUser(); // this is not reqd now coz we have added MemberDetailResolver and below code will retieve data
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+  }
+
+  // loadUser() {
+  //   this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+  //     this.user = user;
+  //   }, error => {
+  //     this.alertify.error(error);
+  //   });
+  // }
+
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+    }
+    return imageUrls;
+  }
+
+}
