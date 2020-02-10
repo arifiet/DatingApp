04ab5c4883588d3ec582ca699e7d8dee791d4aa4
Mohammad Arif <Mohammad.Arif@Aurigo.com>
2020-02-10 08:53:04 +0530

    End of Section 16

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,10 +9,12 @@ namespace DatingApp.API.Data
         {
         }
 
+        // with below variables name tables will be created on migration add
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }  // these are the tables that will be created when migrations are added
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -31,6 +33,17 @@ namespace DatingApp.API.Data
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)      // this is for creating one to many relationship 
                 .OnDelete(DeleteBehavior.Restrict); // among Liker and Likees , a Likee can have many Likers
+
+
+            builder.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
+             builder.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany(m => m.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
 
 
         }
